148a83886962795c4315a46eacea03314932a1a9
Prasenjit10 <prasenjitsasmal1010@gmail.com>
2026-07-11 13:20:07 +0530

    Added server package

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const socket = io(import.meta.env.VITE_SOCKET_URL || 'http://localhost:3001', {
+const socket = io(import.meta.env.VITE_SOCKET_URL || 'https://mind-vs-coin.onrender.com/', {
   autoConnect: false,
   transports: ['websocket']
 });
